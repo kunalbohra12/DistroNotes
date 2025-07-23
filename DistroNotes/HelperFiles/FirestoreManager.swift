dfbc9b1c0c9a25d41a785723d4a8cece99dc6ade
Kunal Bohra <kunalbohrabdapp@gmail.com>
2025-07-23 18:40:35 +0530

    integerate Firebase

---
+++ DistroNotes/HelperFiles/FirestoreManager.swift
@@ -0,0 +1,8 @@
+//
+//  FirestoreManager.swift
+//  DistroNotes
+//
+//  Created by bd013 on 23/07/25.
+//
+
+import Foundation
---
+++ DistroNotes/Model/NotesDataModel/NotesDataModel.swift
@@ -0,0 +1,8 @@
+//
+//  NotesDataModel.swift
+//  DistroNotes
+//
+//  Created by bd013 on 23/07/25.
+//
+
+import Foundation
